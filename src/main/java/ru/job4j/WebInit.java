2aed8f1e38245945e00fcd857d162f97623af3c5
Aliaksandr Zelianko <zely20@mail.ru>
2021-08-02 15:35:49 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/WebInit.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import ru.job4j.WebConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class WebInit implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext servletCxt) {
+        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+        ac.register(WebConfig.class);
+        ac.refresh();
+        DispatcherServlet servlet = new DispatcherServlet(ac);
+        ServletRegistration.Dynamic registration = servletCxt.addServlet("app", servlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
