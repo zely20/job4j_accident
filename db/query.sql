c8befe37437e295aca02a750e258c2e32305a7d3
zely20@mail.ru <Civik1984>
2021-08-04 21:59:57 +0300

    add findAll and create

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,9 @@
-select accident.id, accident.name, types.name FROM accident JOIN types ON types.id = accident.type_id;
+SELECT *
+FROM accident
+         JOIN types
+              ON types.id = accident.type_id
+         LEFT OUTER JOIN rules_accident
+                         ON accident.id = rules_accident.accident_id
+         LEFT OUTER JOIN rules
+                         ON rules.id = rules_accident.rules_id
+where accident.id = 1;
--- src/main/java/ru/job4j/repository/AccidentJdbcTemplate.java
+++ src/main/java/ru/job4j/repository/AccidentJdbcTemplate.java
@@ -1,12 +1,22 @@
 package ru.job4j.repository;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.job4j.model.Accident;
 import ru.job4j.model.AccidentType;
 import ru.job4j.model.Rule;
 
+import javax.persistence.GeneratedValue;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public class AccidentJdbcTemplate {
@@ -17,21 +27,52 @@ public class AccidentJdbcTemplate {
     }
 
     public Accident save(Accident accident, String [] ids) {
-        jdbc.update("insert into accident (name, type_id) values (?, ?)",
-                accident.getName(), accident.getType().getId());
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbc.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement statement = connection.prepareStatement("insert into accident (name, type_id) values (?, ?);", Statement.RETURN_GENERATED_KEYS);
+                statement.setString(1, accident.getName());
+                statement.setInt(2, accident.getType().getId());
+                return statement;
+            }
+        }, keyHolder);
         for (String str : ids) {
             jdbc.update("insert into rules_accident (accident_id, rules_id) values (?, ?)",
-                    accident.getId(), Integer.parseInt(str));
+                    keyHolder.getKeys().get("id"), Integer.parseInt(str));
         }
         return accident;
     }
 
     public List<Accident> getAll() {
-        return jdbc.query("select id, name from accident",
+        return jdbc.query("SELECT " +
+                        "accident.id AS accident_id, " +
+                        "accident.name as accident_name, " +
+                        "types.id as types_id, " +
+                        "types.name as types_name " +
+                        "FROM accident " +
+                        "         JOIN types " +
+                        "              ON types.id = accident.type_id;",
                 (rs, row) -> {
                     Accident accident = new Accident();
-                    accident.setId(rs.getInt("id"));
-                    accident.setName(rs.getString("name"));
+                    AccidentType accidentType = new AccidentType();
+                    accident.setId(rs.getInt("accident_id"));
+                    accident.setName(rs.getString("accident_name"));
+                    accidentType.setId(rs.getInt("types_id"));
+                    accidentType.setName(rs.getString("types_name"));
+                    accident.setType(accidentType);
+                    List<Rule> rules = jdbc.query("select * " +
+                                    "from rules_accident " +
+                                    "join rules " +
+                                    " ON rules_accident.rules_id = rules.id " +
+                                    "where rules_accident.accident_id = " + accident.getId() + ";",
+                            (r,ro) -> {
+                            Rule rule = new Rule();
+                            rule.setId(r.getInt("id"));
+                            rule.setName(r.getString("name"));
+                            return rule;
+                            });
+                    accident.setRules(new HashSet<>(rules));
                     return accident;
                 });
     }
