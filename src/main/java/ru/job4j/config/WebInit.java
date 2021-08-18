42934150312f3b44d92e87060195caab4706188f
zely20@mail.ru <Civik1984>
2021-08-18 20:22:24 +0300

    Spring Security [#6879]

--- src/main/java/ru/job4j/config/WebInit.java
+++ src/main/java/ru/job4j/config/WebInit.java
@@ -14,7 +14,7 @@ public class WebInit implements WebApplicationInitializer {
 
     public void onStartup(ServletContext servletCxt) {
         AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
-        ac.register(WebConfig.class, DataConfig.class);
+        ac.register(WebConfig.class, DataConfig.class, SecurityConfig.class);
         ac.refresh();
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         filter.setEncoding("UTF-8");
