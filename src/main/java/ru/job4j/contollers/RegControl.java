50b28cf9d98081b0a73b7378769a1cd978dea6ba
zely20@mail.ru <Civik1984>
2021-08-23 20:42:44 +0300

    dd user validation

--- src/main/java/ru/job4j/contollers/RegControl.java
+++ src/main/java/ru/job4j/contollers/RegControl.java
@@ -1,10 +1,10 @@
 package ru.job4j.contollers;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.entity.Accident;
 import ru.job4j.entity.User;
 import ru.job4j.repository.AuthorityRepository;
@@ -23,6 +23,22 @@ public class RegControl {
         this.authorities = authorities;
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    public String userAlreadyReg() {
+        return "redirect:/reg?regError=true";
+    }
+
+    @GetMapping("/reg")
+    public String reg(@RequestParam(value = "regError", required = false) String regError,
+                      Model model) {
+        String errorMessage = null;
+        if (regError != null) {
+            errorMessage = "Такой пользователь уже зарегистрирован";
+        }
+        model.addAttribute("error", errorMessage);
+        return "reg";
+    }
+
     @PostMapping("/reg")
     public String save(@ModelAttribute User user) {
         user.setEnabled(true);
@@ -30,10 +46,5 @@ public class RegControl {
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
         users.save(user);
         return "redirect:/login";
-    }
-
-    @GetMapping("/reg")
-    public String reg(@ModelAttribute Accident accident) {
-        return "reg";
     }
 }
--- src/main/webapp/WEB-INF/views/reg.jsp
+++ src/main/webapp/WEB-INF/views/reg.jsp
@@ -1,7 +1,15 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <html>
 <body>
+<div class="container px-4" >
+<c:if test="${not empty error}">
+    <div style="color:red; font-weight: bold; margin: 30px 0px;">
+            ${error}
+    </div>
+</c:if>
 <form name='login' action="<c:url value='/reg'/>" method='POST'>
     <table>
         <tr>
@@ -17,5 +25,6 @@
         </tr>
     </table>
 </form>
+</div>
 </body>
 </html>
